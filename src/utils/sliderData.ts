1dc2c71b7c1404e40928437b12b33764250627bf
noursallam <noursallam396@gmail.com>
2025-07-11 14:38:20 +0300

    forceing like a bomb

---
+++ src/utils/sliderData.ts
@@ -0,0 +1,23 @@
+// Images for the services slider section
+const sliderServices = [
+    {
+        title: "التصميم الجرافيكي",
+        description: "تصميمات إبداعية عالية الجودة تعزز هوية علامتك التجارية وتجذب انتباه جمهورك المستهدف",
+        mobileImage: "/images/design.png",
+        desktopImage: "/images/design-بالعرض.png",
+    },
+    {
+        title: "الإنتاج الإعلامي",
+        description: "خدمات إنتاج فيديو وصور  احترافية لتوصيل رسالتك بشكل مؤثر وجذاب للجمهور المستهدف",
+        mobileImage: "/images/MEDIA-PRO.png",
+        desktopImage: "/images/media-pro-بالعرض.png",
+    },
+    {
+        title: "تطوير الهوية البصرية",
+        description: "بناء هوية بصرية متكاملة تعكس قيم علامتك التجارية وتميزها عن المنافسين في السوق",
+        mobileImage: "/images/branding.png",
+        desktopImage: "/images/branding-بالعرض.png",
+    }
+];
+
+export { sliderServices };
---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: "class",
+	theme: {
+		extend: {
+			colors: {
+				body: "rgb(var(--color-bg))",
+				"box-bg": "rgb(var(--color-box))",
+				"box-shadow": "rgb(var(--box-sd))",
+				"box-border": "rgb(var(--box-border))",
+				primary: "#1d4ed8",
+				"heading-1": "rgb(var(--heading-1))",
+				"heading-2": "rgb(var(--heading-2))",
+				"heading-3": "rgb(var(--heading-3))",
+			},
+			fontFamily: {
+				alexandria: ['Alexandria', 'sans-serif'],
+				zawi: ['29LT Zawi', 'Alexandria', 'sans-serif'],
+			},
+			screens:{
+				midmd:"880px"
+			}
+		},
+	},
+	plugins: [],
+}
